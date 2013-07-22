ef7f973b7c45fec5d2df0d94690b466f9b18a4db
Dustin Oprea <myselfasunder@gmail.com>
2013-07-22 02:50:54 -0400

    Added verbosity option to push tools.

--- p_m/tools/pm_push_dpkg.py
+++ p_m/tools/pm_push_dpkg.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
 from sys import exit
+from argparse import ArgumentParser
 
 from p_m.system.system_types import get_system_imps, SYS_UBUNTU
 from p_m.system.repo_types import get_repo_imps, SYS_DPKG
 from p_m.client import Client
+
+parser = ArgumentParser(description='Package-list push for DPKG.')
+parser.add_argument('-v', '--verbose', action='store_true', help="Show logging.")
+
+result = parser.parse_args()
+
+if result.verbose:
+    from p_m import logging_config
 
 system_profiler = get_system_imps(SYS_UBUNTU)
 package_list_getter = get_repo_imps(SYS_DPKG)
