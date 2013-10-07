45b75680180d82dd5344b4c399c0197298a7bc69
Dustin Oprea <myselfasunder@gmail.com>
2013-10-07 06:30:53 -0400

    Renamed environment variables.

--- dev/pushlist_dpkg.sh
+++ dev/pushlist_dpkg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-PM_PREFS_PATH=~/.package_backup.dev PM_API_URL_PREFIX=http://localhost:8080 PYTHONPATH=.:../pysecure pmclient/tools/pm_pushlist_dpkg.py
+PB_PREFS_PATH=~/.package_backup.dev PB_API_URL_PREFIX=http://localhost:8080 PYTHONPATH=.:../pysecure pbclient/tools/pb_pushlist_dpkg.py
 
--- pbclient/config.py
+++ pbclient/config.py
@@ -3,7 +3,7 @@ from os.path import expandvars
 
 CLIENT_ID = 'e9d891152bb45b453111fbc4bda67f82c180450ebf50cc2ca265e96b82b149f1'\
             '00e06'
-PREFS_PATH = environ.get('PM_PREFS_PATH', 
+PREFS_PATH = environ.get('PB_PREFS_PATH', 
                          expandvars('$HOME/.package_backup'))
 PREFS_FILENAME = 'prefs.json'
 LOCK_FILEPATH = '/tmp/pm_prefs.lock'
@@ -13,7 +13,7 @@ API_AUTH_KEY_HEADER = 'X-Api-Auth-Key'
 API_AUTH_SECRET_HEADER = 'X-Api-Auth-Secret'
 
 API_DEFAULT_URL_PREFIX = 'https://www.packagebackup.com'
-API_URL_PREFIX = environ.get('PM_API_URL_PREFIX', 
+API_URL_PREFIX = environ.get('PB_API_URL_PREFIX', 
                              API_DEFAULT_URL_PREFIX)
 
 API_URL_LIST_PUSH = ('%s%s' % (API_URL_PREFIX, '/api/list/push'))
