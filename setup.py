39ad0d6a20aa9d3d385f46bb5b87a39ad09f3946
Dustin Oprea <myselfasunder@gmail.com>
2013-10-10 20:02:07 -0400

    Was incorrectly showing prompt defaults.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.command.install import install
 
 from pbclient.install_phases import pre_install, post_install
 
-version = '0.5.3'
+version = '0.5.4'
 
 
 class custom_install(install):
